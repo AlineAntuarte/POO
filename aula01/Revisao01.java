abf9249b2ccefc7cdd32c75d0839957675a33325
AlineAntuarte <aline.ba@aluno.ifsc.edu.br>
2026-04-14 21:29:04 -0300

    AULA 01 — Exercício 1: Revisão de condicionais

--- aula01/Revisao01.java
+++ aula01/Revisao01.java
@@ -14,11 +14,31 @@ public class Revisao01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("=== Verificador de notas ===");
+        System.out.print("=== Insira uma nota: ");
 
         // TODO: use um loop 'while' ou 'do-while'
         // Leia a nota com sc.nextDouble()
         // Se a nota == -1, saia do loop
         // Caso contrário, imprima a situação
+
+        double nota = sc.nextDouble();
+        boolean chave = true;
+
+        System.out.println("Sua nota é: " + nota);
+
+        while (chave) {
+            if (nota < 0 || nota > 10) {
+                chave = false;
+            } else {
+                if (nota < 6) {
+                    System.out.println("Você não está aprovado!");
+                    chave = false;
+                } else {
+                    System.out.println("Você está aprovado!");
+                    chave = false;
+                }
+            }
+        }
 
         System.out.println("Encerrando.");
         sc.close();
