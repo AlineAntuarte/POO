2b2be23d7ea4111e4078efa805694cff120d6da0
AlineAntuarte <aline.ba@aluno.ifsc.edu.br>
2026-04-27 20:08:31 -0300

    Aprendendo

--- aula02/Ex01Produto.java
+++ aula02/Ex01Produto.java
@@ -15,11 +15,11 @@ import java.util.List;
 
 /**
  * Skins Vandal - Valores Individuais
- * | Nome              | Categoria | Valor (VP) | Valor (R$) |
+ * | Nome | Categoria | Valor (VP) | Valor (R$) |
  * |-------------------|-----------|------------|------------|
- * | ORA (OneTap)      | Ultra     | 2.475      | ~ R$ 92,00 |
- * | Proteção Fásica   | Ultra     | 2.475      | ~ R$ 92,00 |
- * | Prelúdio do Caos  | Exclusiva | 2.175      | ~ R$ 75,00 |
+ * | ORA (OneTap) | Ultra | 2.475 | ~ R$ 92,00 |
+ * | Proteção Fásica | Ultra | 2.475 | ~ R$ 92,00 |
+ * | Prelúdio do Caos | Exclusiva | 2.175 | ~ R$ 75,00 |
  */
 
 class Produto {
@@ -30,8 +30,21 @@ class Produto {
     int quantidade;
 
     // TODO: adicione o método calcularTotal() que retorna preco * quantidade
+    double calcularTotal() {
+        return preco * quantidade;
+    }
+
     // TODO: adicione o método mostrarInfo() que imprime nome, preco e total
+    void mostrarInfo() {
+        System.out.println("Nome:" + nome + " | " + " Preço: R$" + preco + " | " + "Total: R$" + calcularTotal());
+    }
+
     // TODO: adicione o @Override toString()
+    // O toString ajuda a imprimir o objeto diretamente se precisar
+    @Override
+    public String toString() {
+
+    }
 }
 
 public class Ex01Produto {
